e806dce64b1b7bbd6c0d5fa08ee915336671e5ea
unknown <guillaume.gamelas@gmail.com>
2018-07-01 00:25:44 +0200

    12. COMPLETE (Setup Collision and Overlap Event)

--- Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -2,6 +2,8 @@
 
 #include "FPSObjectiveActor.h"
 #include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 
 // Sets default values
@@ -11,9 +13,13 @@ AFPSObjectiveActor::AFPSObjectiveActor()
 	PrimaryActorTick.bCanEverTick = true;
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	RootComponent = MeshComp;
 
 	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 	SphereComp->SetupAttachment(MeshComp);
 }
 
@@ -22,6 +28,12 @@ void AFPSObjectiveActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	PlayEffects();
+}
+
+void AFPSObjectiveActor::PlayEffects()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(this, PickupFX, GetActorLocation());
 }
 
 // Called every frame
@@ -29,5 +41,12 @@ void AFPSObjectiveActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	PlayEffects();
 }
 
